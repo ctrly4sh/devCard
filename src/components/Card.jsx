135814494b6096453787a99fb0f4b4d73441847e
yash tiwari <149458186+ctrly4sh@users.noreply.github.com>
2024-06-16 23:25:47 +0530

    toggle added : theme swticher

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -38,7 +38,7 @@ export default function Card() {
                         href="https://dev.to/y4sh" target="_blank"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        dev.to
+                        Dev.to
                     </a>
                     <a
                         href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=y4sh.dev@gmail.com" target="_blank"
